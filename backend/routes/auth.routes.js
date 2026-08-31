82b25a836329754a7bbfe07fee0bf9e349df6753
Mia <z5592248@student.unsw.edu.au>
2026-08-31 22:23:56 +0700

    deleteAccount auth func

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup, login, logout } from '../auth.js';
+import { signup, login, logout, deleteAccount } from '../auth.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
+router.delete('/account', deleteAccount);
 
 export default router;
